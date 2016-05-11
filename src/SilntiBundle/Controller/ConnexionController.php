af75031abf132deb19381f780cad5d5f7a7bf58b
Alain DRILLON <contact@drillon-ala.in>
2016-05-11 13:02:26 +0200

    Formulaire Symfony

--- src/SilntiBundle/Controller/ConnexionController.php
+++ src/SilntiBundle/Controller/ConnexionController.php
@@ -4,6 +4,9 @@ namespace SilntiBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ConnexionController extends Controller
 {
@@ -12,7 +15,13 @@ class ConnexionController extends Controller
 	 */
 	public function connexionAction()
 	{
-		$html = $this->container->get('templating')->render('SilntiBundle::Default/connexion.html.twig');
+        $form = $this->createFormBuilder(null)->add('username', TextType::class, array('label' => 'Nom d\'utilisateur'))
+                                              ->add('password', PasswordType::class, array('label' => 'Mot de passe'))
+                                              ->add('login', SubmitType::class, array('label' => 'Connexion'))
+                                              ->getForm();
+
+		$html = $this->container->get('templating')->render('SilntiBundle::Default/connexion.html.twig',
+			array('form_connexion' => $form->createView()));
 		return new Response($html);
 	}
 }
